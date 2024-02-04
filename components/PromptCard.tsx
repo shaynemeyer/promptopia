17ce07b62878108c962a8d4e345ae7e1d494edbe
Shayne Meyer <shayne.meyer@gmail.com>
2024-02-04 13:15:28 -0800

    initial commit

---
+++ components/PromptCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PromptCard() {
+  return <div>PromptCard</div>;
+}
+
+export default PromptCard;
---
+++ components/Provider.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { SessionProvider } from 'next-auth/react';
+
+function Provider({
+  children,
+  session,
+}: {
+  children: React.ReactNode;
+  session: any;
+}) {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
+}
+
+export default Provider;

226d241797f6a13d74ef4a252d59b892414fb963
Shayne Meyer <shayne.meyer@gmail.com>
2024-02-06 12:48:51 -0800

    added create-prompt

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -12,36 +12,40 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session?.user?.email,
+      });
 
-    session.user.id = sessionUser._id.toString();
-
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
-
-      // check if a user already exists
-      const userExists = await User.findOne({ email: profile.email });
-
-      // if not, create a new user
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(' ', '').toLowerCase(),
-          image: profile.picture,
-        });
+      if (session.user) {
+        session.user.id = sessionUser._id.toString();
       }
 
-      return true;
-    } catch (err) {
-      console.log(err);
-      return false;
-    }
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+
+        // check if a user already exists
+        const userExists = await User.findOne({ email: profile?.email });
+
+        // if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile?.email,
+            username: profile?.name?.replace(' ', '').toLowerCase(),
+            image: profile?.picture,
+          });
+        }
+
+        return true;
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    },
   },
 });
 

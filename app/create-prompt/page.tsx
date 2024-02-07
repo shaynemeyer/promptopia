e103833ed9be3dc250bda10431b3039e92d87336
Shayne Meyer <shayne.meyer@gmail.com>
2024-02-06 19:11:22 -0800

    added feeds, prompts and profile

--- app/create-prompt/page.tsx
+++ app/create-prompt/page.tsx
@@ -5,13 +5,38 @@ import { useRouter } from 'next/navigation';
 import Form from '@/components/Form';
 
 function CreatePrompt() {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: '',
     tag: '',
   });
 
-  const createPrompt = (e) => {};
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt || '',
+          userId: session?.user?.id,
+          tag: post.tag || '',
+        }),
+      });
+
+      if (response.ok) {
+        router.push('/');
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
